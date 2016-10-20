5cd95bc24357182cb13614fadeb36ba336207034
js leleu <jsleleu@gmail.com>
2016-10-20 09:14:00 +0200

    Chap 2
    essai chap 2

---
+++ src/sdz1.java
@@ -0,0 +1,19 @@
+
+public class sdz1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+		System.out.println("My name is");
+		System.out.println("Cysboy");
+		float temperature;
+		temperature = 64;
+		System.out.println(temperature);
+		double nbre1=3, nbre2 = 2;
+		double resultat = nbre1/nbre2; 
+		System.out.println(resultat);
+		double  nombre= 1__000_000; 
+		System.out.println(nombre);
+	}
+
+}
