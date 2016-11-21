84d5b9fa7fd01e38396c4e3d3eec1ba7c7bceb6e
js leleu <jsleleu@gmail.com>
2016-11-21 07:28:44 +0100

    Calcul des conversions
    Ok, avec arrondi

--- src/sdz1.java
+++ src/sdz1.java
@@ -12,24 +12,33 @@ public class sdz1 {
 	for (int i=0;i<num;i++){
 		System.out.print("-");
 	}
-	System.out.println();
-	System.out.println("Choisissez le mode de conversion : ");
-	System.out.println("1- Convertisseur Celsius-Fahrenheit");
-	System.out.println("2- Convertisseur Fahrenheit-Celsius");
-	int i = sc.nextInt();
-	sc.nextLine();
-	//bien penser à remettre c comme variable pour la réponse (O/N)
+	
 	char c ='O';
 	while (c=='O'){
+		System.out.println();
+		System.out.println("Choisissez le mode de conversion : ");
+		System.out.println("1- Convertisseur Celsius-Fahrenheit");
+		System.out.println("2- Convertisseur Fahrenheit-Celsius");
+		int i = sc.nextInt();
+		sc.nextLine();
+		//bien penser à remettre c comme variable pour la réponse (O/N);
+		
 	System.out.println("Température à convertir");
 	double old = sc.nextDouble();
 	if (i==1) {
+		double fahr = 9*old/5+32; 
+		fahr = arrondi(fahr,2);
+		System.out.println(old + "°C correspond à "+ fahr+ "°F");
 		//de celsius à fahrenheit
 	}
 	else {
+		double cels = (old-32)*5/9; 
+		cels = arrondi(cels,2);
+		System.out.println(old + "°F correspond à "+ cels+ "°C");
 		//de fahrenheit à celsius
 	}
 	System.out.println("Souhaitez vous convertir une autre température ? (O/N)");
+	sc.nextLine();
 	String temp  = sc.nextLine();
 	c = temp.charAt(0);
 	}
