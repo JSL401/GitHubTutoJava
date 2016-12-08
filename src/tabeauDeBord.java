198e1614f53c465afb83ce74a60700ee39022c14
js leleu <jsleleu@gmail.com>
2016-12-08 17:48:23 +0100

    remplir plusieurs factures UGAP
    Yes !

--- src/tabeauDeBord.java
+++ src/tabeauDeBord.java
@@ -11,24 +11,45 @@ public class tabeauDeBord {
 		Scanner sc= new Scanner(System.in);
 		tableauMois[0]=LocalDate.of(2016, 1, 1);
 		Locale.setDefault(Locale.FRENCH);
+		int j= 0; 
 		
 		// remplissage années et mois
 		for(int i=1;i<47;i++){
 			tableauMois[i]=tableauMois[0].plusMonths(i);
 		}
-		//Travail sur une Facture UGAP
+		System.out.println("souhaitez vous ajouter une facture UGAP ? O/N");
+		char reponseUgap = sc.nextLine().charAt(0);
+		
+		//Travail sur Facture UGAP
 		// date de la facture 
-		System.out.println("Quel est la date de cette facture? (01/02/2016)");	
-		LocalDate factureDate = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-		// montant de la facture
-		System.out.println("Quel est le montant de cette facture? (en EUROS HT)");	
-		double factureMontant = sc.nextDouble();
-		//durée de la facture
-		System.out.println("Quelle est la durée de cette facture? (en mois)"); 
-		long duree = sc.nextLong(); 
-		LocalDate factureFin = factureDate.plusMonths(duree); 
-		System.out.println("la facturation se termine le : " + factureFin);
-
+		while (reponseUgap == 'O') {
+		//initialisation des tableaux	
+			LocalDate[] factureDate = new LocalDate[100];
+			double[] factureMontant = new double[100];
+			LocalDate[] factureFin = new LocalDate[100];
+		
+			if (j==100) {
+				System.out.println("Prévenir developpeur qu'il n'y plus de place dans l'Array");
+			} else {
+				System.out.println("Quel est la date de cette facture? (01/02/2016)");	
+				factureDate[j] = LocalDate.parse(sc.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+				// montant de la facture
+				System.out.println("Quel est le montant de cette facture? (en EUROS HT)");	
+				factureMontant[j] = sc.nextDouble();
+				//durée de la facture
+				System.out.println("Quelle est la durée de cette facture? (en mois)"); 
+				long duree = sc.nextLong(); 
+				factureFin[j] = factureDate[j].plusMonths(duree); 
+				System.out.println("la facturation se termine le : " + factureFin[j]);
+				j++;
+				System.out.println("Souhaitez vous ajouter une facture Ugap ? O/N");
+				sc.nextLine();
+				reponseUgap=sc.nextLine().charAt(0);
+			}
+		
+		
+		}
+		
 	}
 
 }
